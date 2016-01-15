60e8d138dae7f6ac939bf7abc781ef782cd09bd9
YoungHui Park <ypark234@wisc.edu>
2016-01-15 14:07:27 -0600

    adding assert_equal and assert_almost_equals

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,14 @@
 from stats import mean
+from nose.tools import assert_equal, assert_almost_equals
 
 def test_mean():
-	assert mean([2,4]) == 3.0
-test_mean()
+	assert_equal(mean([2,4]), 3)
+#test_mean()
 
 def test_empty_list():
 	assert mean([]) == 0
-test_empty_list()
+#test_empty_list()
 
 def test_float_mean():
-	assert mean([.5,.5,1]) == 0.6666
-test_float_mean()
+	assert_almost_equals(mean([.5,.5,1]), .66, 1) # .66, 1-> can choose how close by degree
+#test_float_mean()
